a374c90ee84dc621a585a9088b11e1b72edad9f8
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-03-17 00:04:42 +0900

    added getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,36 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import * as middy from 'middy'
+import cors from '@middy/http-cors'
+import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+const docClient = new DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Get all TODO items for a current user
+    const userId = getUserId(event)
+
+    const result = await docClient
+      .scan({
+        TableName: todosTable
+      })
+      .promise()
+
+    const items = result.Items
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(items)
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
