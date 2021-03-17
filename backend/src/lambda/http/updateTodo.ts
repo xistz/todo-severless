efff9f56db516a127ca1e42e01dbcdc914a09fb6
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-03-17 23:34:41 +0900

    added updateTodo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,51 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import * as middy from 'middy'
+import cors from '@middy/http-cors'
+import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+const docClient = new DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    const userId = getUserId(event)
+
+    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+    const result = await docClient
+      .update({
+        TableName: todosTable,
+        Key: {
+          todoId,
+          userId
+        },
+        UpdateExpression: 'set name = :name, dueDate = :dueDate, done = :done',
+        ExpressionAttributeValues: {
+          ':name': updatedTodo.name,
+          ':dueDate': updatedTodo.dueDate,
+          ':done': updatedTodo.done
+        },
+        ReturnValues: 'ALL_NEW'
+      })
+      .promise()
+
+    const item = result.Attributes
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item)
+    }
+  }
+)
+
+handler.use(cors({ credentials: true }))
