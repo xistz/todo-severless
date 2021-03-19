5831d8b405496043498f2b06a73a2a0fb31add5c
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-03-19 23:28:40 +0900

    added xray to getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,12 +1,14 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import * as middy from 'middy'
 import cors from '@middy/http-cors'
 import { getUserId } from '../utils'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
-const docClient = new DocumentClient()
+const XAWS = AWSXRay.captureAWS(AWS)
+const docClient = new XAWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
 
 export const handler = middy(
@@ -14,23 +16,27 @@ export const handler = middy(
     // TODO: Get all TODO items for a current user
     const userId = getUserId(event)
 
-    const result = await docClient
-      .query({
-        TableName: todosTable,
-        KeyConditionExpression: 'UserId = :userId',
-        ExpressionAttributeValues: {
-          ':userId': userId
-        }
-      })
-      .promise()
-
-    const items = result.Items
+    const todos = getTodos(userId)
 
     return {
       statusCode: 200,
-      body: JSON.stringify(items)
+      body: JSON.stringify({ todos })
     }
   }
 )
 
 handler.use(cors({ credentials: true }))
+
+const getTodos = async (userId: string) => {
+  const result = await docClient
+    .query({
+      TableName: todosTable,
+      KeyConditionExpression: 'UserId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId
+      }
+    })
+    .promise()
+
+  return result.Items
+}
